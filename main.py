897e52bc2c6d3ec9d85a6e848567fc35e7d0eea9
tiiedye <tiiedye@gmail.com>
2021-11-25 09:14:25 -0800

    adding more usability, ability to encode/decode sentances

--- main.py
+++ main.py
@@ -3,26 +3,42 @@ import art
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
+alphabet_len = len(alphabet)
 
 
 def encrypt(message, spaces):
-    alphabet_len = len(alphabet)
     output = ""
+    shift_int = int(spaces)
+
+    if shift_int > alphabet_len:
+        shift_int = shift_int % alphabet_len
+
     for i in message:
-        index = alphabet.index(i)
-        if index + int(spaces) > alphabet_len:
-            new_shift = (index + int(spaces)) - alphabet_len
-            output += alphabet[new_shift]
+        if i in alphabet:
+            index = alphabet.index(i)
+            if index + shift_int > alphabet_len:
+                new_shift = (index + shift_int) - alphabet_len
+                output += alphabet[new_shift]
+            else:
+                output += alphabet[index + shift_int]
         else:
-            output += alphabet[index + int(spaces)]
+            output += i
     print(f"The encoded text is: {output}")
 
 
 def decrypt(message, spaces):
     output = ""
+    shift_int = int(spaces)
+
+    if shift_int > alphabet_len:
+        shift_int = shift_int % alphabet_len
+
     for i in message:
-        index = alphabet.index(i)
-        output += alphabet[index - int(spaces)]
+        if i in alphabet:
+            index = alphabet.index(i)
+            output += alphabet[index - shift_int]
+        else:
+            output += i
     print(f"The decoded text is: {output}")
 
 
